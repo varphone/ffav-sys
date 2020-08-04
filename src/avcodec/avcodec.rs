54c99e696440e0e046deeff27e37a381e4a8dd01
Varphone Wong <varphone@qq.com>
2020-08-04 17:04:31 +0800

    impl Default for AVPacket
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/avcodec/avcodec.rs
+++ src/avcodec/avcodec.rs
@@ -1,8 +1,27 @@
-use crate::{AVCodecID,AVPixelFormat,AVSampleFormat};
+use crate::{AVCodecID, AVPacket, AVPixelFormat, AVSampleFormat, AV_NOPTS_VALUE};
 
 impl Default for AVCodecID {
     fn default() -> Self {
         AVCodecID::AV_CODEC_ID_NONE
+    }
+}
+
+impl Default for AVPacket {
+    fn default() -> Self {
+        Self {
+            buf: std::ptr::null_mut(),
+            pts: AV_NOPTS_VALUE,
+            dts: AV_NOPTS_VALUE,
+            data: std::ptr::null_mut(),
+            size: 0,
+            stream_index: 0,
+            flags: 0,
+            side_data: std::ptr::null_mut(),
+            side_data_elems: 0,
+            duration: 0,
+            pos: -1,
+            convergence_duration: 0,
+        }
     }
 }
 
@@ -11,7 +30,6 @@ impl Default for AVPixelFormat {
         AVPixelFormat::AV_PIX_FMT_NONE
     }
 }
-
 
 impl Default for AVSampleFormat {
     fn default() -> Self {
