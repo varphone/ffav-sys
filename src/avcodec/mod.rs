ac1a048e35634fe24e11dddb02cce8ce004026a2
Varphone Wong <varphone@qq.com>
2020-08-12 22:54:19 +0800

    avcodec: Add impl AVCodecContext and AVPacket
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/avcodec/mod.rs
+++ src/avcodec/mod.rs
@@ -1,4 +1,30 @@
-use crate::{AVCodecID, AVPacket, AVPixelFormat, AVSampleFormat, AV_NOPTS_VALUE};
+use crate::{
+    AVCodecContext, AVCodecID, AVPacket, AVPacketSideData, AVPixelFormat, AVSampleFormat,
+    AV_NOPTS_VALUE,
+};
+use std::convert::TryInto;
+
+impl AVCodecContext {
+    /// Some codecs need / can use extradata like Huffman tables.
+    #[inline]
+    pub fn extradata(&self) -> &[u8] {
+        unsafe {
+            std::slice::from_raw_parts(self.extradata, self.extradata_size.try_into().unwrap())
+        }
+    }
+
+    /// Additional data associated with the entire coded stream.
+    #[inline]
+    pub fn coded_side_data(&self) -> &[AVPacketSideData] {
+        if self.coded_side_data.is_null() || self.nb_coded_side_data <= 0 {
+            &[]
+        } else {
+            unsafe {
+                std::slice::from_raw_parts(self.coded_side_data, self.nb_coded_side_data as usize)
+            }
+        }
+    }
+}
 
 impl Default for AVCodecID {
     fn default() -> Self {
@@ -25,6 +51,28 @@ impl Default for AVPacket {
     }
 }
 
+impl AVPacket {
+    /// Returns true if data bytes has a length of zero bytes.
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Returns the length of data bytes.
+    pub fn len(&self) -> usize {
+        self.size as usize
+    }
+
+    /// Converts a data ptr to a byte slice.
+    pub fn as_bytes(&self) -> &[u8] {
+        unsafe { std::slice::from_raw_parts(self.data, self.size as usize) }
+    }
+
+    /// Converts a mutable data ptr to a mutable byte slice.
+    pub fn as_bytes_mut(&mut self) -> &mut [u8] {
+        unsafe { std::slice::from_raw_parts_mut(self.data, self.size as usize) }
+    }
+}
+
 impl Default for AVPixelFormat {
     fn default() -> Self {
         AVPixelFormat::AV_PIX_FMT_NONE
@@ -34,5 +82,19 @@ impl Default for AVPixelFormat {
 impl Default for AVSampleFormat {
     fn default() -> Self {
         AVSampleFormat::AV_SAMPLE_FMT_NONE
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_avpacket() {
+        let mut pkt = AVPacket::default();
+        assert_eq!(pkt.is_empty(), true);
+        assert_eq!(pkt.len(), 0);
+        assert_eq!(pkt.as_bytes(), &[]);
+        assert_eq!(pkt.as_bytes_mut(), &[]);
     }
 }
