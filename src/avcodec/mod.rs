ab8a16cd054480bdd5efc5fa3c90b719876cd7c9
Varphone Wong <varphone@qq.com>
2020-08-12 17:17:28 +0800

    Prepare for next
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/avcodec/mod.rs
+++ src/avcodec/mod.rs
@@ -1,4 +1,33 @@
-use crate::{AVCodecID, AVPacket, AVPixelFormat, AVSampleFormat, AV_NOPTS_VALUE};
+use crate::{
+    AVCodecContext, AVCodecID, AVPacket, AVPacketSideData, AVPixelFormat, AVSampleFormat,
+    AV_NOPTS_VALUE,
+};
+use std::convert::TryInto;
+
+impl AVCodecContext {
+    /// Some codecs need / can use extradata like Huffman tables.
+    #[inline]
+    pub fn extradata(&self) -> &[u8] {
+        unsafe {
+            std::slice::from_raw_parts(self.extradata, self.extradata_size.try_into().unwrap())
+        }
+    }
+
+    /// Additional data associated with the entire coded stream.
+    #[inline]
+    pub fn coded_side_data(&self) -> &[AVPacketSideData] {
+        if self.coded_side_data.is_null() || self.nb_coded_side_data <= 0 {
+            &[]
+        } else {
+            unsafe {
+                std::slice::from_raw_parts(
+                    self.coded_side_data,
+                    self.nb_coded_side_data.try_into().unwrap(),
+                )
+            }
+        }
+    }
+}
 
 impl Default for AVCodecID {
     fn default() -> Self {
@@ -22,6 +51,28 @@ impl Default for AVPacket {
             pos: -1,
             convergence_duration: 0,
         }
+    }
+}
+
+impl AVPacket {
+    /// Returns true if data bytes has a length of zero bytes.
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Returns the length of data bytes.
+    pub fn len(&self) -> usize {
+        self.size as usize
+    }
+
+    /// Converts a data ptr to a byte slice.
+    pub fn as_bytes(&self) -> &[u8] {
+        unsafe { std::slice::from_raw_parts(self.data, self.size as usize) }
+    }
+
+    /// Converts a mutable data ptr to a mutable byte slice.
+    pub fn as_bytes_mut(&self) -> &mut [u8] {
+        unsafe { std::slice::from_raw_parts_mut(self.data, self.size as usize) }
     }
 }
 
--- src/avformat/mod.rs
+++ src/avformat/mod.rs
@@ -1,16 +1,23 @@
-use crate::{AVChapter, AVFormatContext, AVProgram, AVStream};
+use crate::{
+    AVChapter, AVCodecContext, AVCodecParameters, AVDictionary, AVFormatContext, AVPacketSideData,
+    AVProgram, AVStream,
+};
+use std::convert::TryInto;
 
 impl AVFormatContext {
+    /// Number of elements in AVFormatContext.streams.
     #[inline]
     pub fn nb_streams(&self) -> usize {
         self.nb_streams as usize
     }
 
+    /// A list of all streams in the file.
     #[inline]
     pub fn streams(&self) -> &[&AVStream] {
         unsafe { std::slice::from_raw_parts(self.streams as *const &AVStream, self.nb_streams()) }
     }
 
+    /// A list of all streams in the file.
     #[inline]
     pub fn streams_mut(&self) -> &[&mut AVStream] {
         unsafe {
@@ -18,11 +25,13 @@ impl AVFormatContext {
         }
     }
 
+    /// Number of elements in AVFormatContext.programs.
     #[inline]
     pub fn nb_programs(&self) -> usize {
         self.nb_programs as usize
     }
 
+    /// A list of all programs in the file.
     #[inline]
     pub fn programs(&self) -> &[&AVProgram] {
         unsafe {
@@ -30,6 +39,7 @@ impl AVFormatContext {
         }
     }
 
+    /// A list of all programs in the file.
     #[inline]
     pub fn programs_mut(&self) -> &[&mut AVProgram] {
         unsafe {
@@ -37,11 +47,13 @@ impl AVFormatContext {
         }
     }
 
+    /// Number of elements in AVFormatContext.chapters.
     #[inline]
     pub fn nb_chapters(&self) -> usize {
         self.nb_chapters as usize
     }
 
+    /// A list of all chapters in the file.
     #[inline]
     pub fn chapters(&self) -> &[&AVChapter] {
         unsafe {
@@ -49,10 +61,51 @@ impl AVFormatContext {
         }
     }
 
+    /// A list of all chapters in the file.
     #[inline]
     pub fn chapters_mut(&self) -> &[&mut AVChapter] {
         unsafe {
             std::slice::from_raw_parts(self.chapters as *const &mut AVChapter, self.nb_chapters())
+        }
+    }
+}
+
+impl AVStream {
+    #[inline]
+    pub fn codec(&self) -> Option<&AVCodecContext> {
+        if self.codec.is_null() {
+            None
+        } else {
+            unsafe { Some(&*self.codec) }
+        }
+    }
+
+    #[inline]
+    pub fn codecpar(&self) -> Option<&AVCodecParameters> {
+        if self.codecpar.is_null() {
+            None
+        } else {
+            unsafe { Some(&*self.codecpar) }
+        }
+    }
+
+    #[inline]
+    pub fn metadata(&self) -> Option<&AVDictionary> {
+        if self.metadata.is_null() {
+            None
+        } else {
+            unsafe { Some(&*self.metadata) }
+        }
+    }
+
+    #[inline]
+    pub fn side_data(&self) -> &[AVPacketSideData] {
+        if self.side_data.is_null() || self.nb_side_data <= 0 {
+            &[]
+        } else {
+            unsafe {
+                std::slice::from_raw_parts(self.side_data, self.nb_side_data.try_into().unwrap())
+            }
         }
     }
 }
