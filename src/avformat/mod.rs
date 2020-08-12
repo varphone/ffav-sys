ee2d6c0b76a72c7637d9d29559200c47529697a4
Varphone Wong <varphone@qq.com>
2020-08-12 22:46:16 +0800

    avformat: Add impl AVStream
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/avformat/mod.rs
+++ src/avformat/mod.rs
@@ -1,4 +1,8 @@
-use crate::{AVChapter, AVFormatContext, AVProgram, AVStream};
+use crate::{
+    AVChapter, AVCodecContext, AVCodecParameters, AVDictionary, AVFormatContext, AVPacketSideData,
+    AVProgram, AVStream,
+};
+use std::convert::TryInto;
 
 impl AVFormatContext {
     #[inline]
@@ -53,6 +57,50 @@ impl AVFormatContext {
     pub fn chapters_mut(&self) -> &[&mut AVChapter] {
         unsafe {
             std::slice::from_raw_parts(self.chapters as *const &mut AVChapter, self.nb_chapters())
+        }
+    }
+}
+
+impl AVStream {
+    /// The context of the encoded stream.
+    #[inline]
+    pub fn codec(&self) -> Option<&AVCodecContext> {
+        if self.codec.is_null() {
+            None
+        } else {
+            unsafe { Some(&*self.codec) }
+        }
+    }
+
+    /// The properties of the encoded stream.
+    #[inline]
+    pub fn codecpar(&self) -> Option<&AVCodecParameters> {
+        if self.codecpar.is_null() {
+            None
+        } else {
+            unsafe { Some(&*self.codecpar) }
+        }
+    }
+
+    /// Metadata of the stream.
+    #[inline]
+    pub fn metadata(&self) -> Option<&AVDictionary> {
+        if self.metadata.is_null() {
+            None
+        } else {
+            unsafe { Some(&*self.metadata) }
+        }
+    }
+
+    /// An array of side data that applies to the stream.
+    #[inline]
+    pub fn side_data(&self) -> &[AVPacketSideData] {
+        if self.side_data.is_null() || self.nb_side_data <= 0 {
+            &[]
+        } else {
+            unsafe {
+                std::slice::from_raw_parts(self.side_data, self.nb_side_data.try_into().unwrap())
+            }
         }
     }
 }
